c9abdd1e4186779a566d8187387422941034fae2
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2023-06-19 01:13:30 -0500

    add download log button

--- scripts/destroyer.js
+++ scripts/destroyer.js
@@ -4,14 +4,19 @@ let iframeDocument
 let allowExportButton 
 let objectIntegratedButton 
 let nextButton 
+let table
 
 let isRunning 
 let runCount 
 let runLimit 
 let delay 
+let logData
+
 
 const destroyer = ()  => {
 
+
+logData = localStorage.getItem("logData") ? localStorage.getItem("logData") : "";
 isRunning = localStorage.getItem("isRunning") ? JSON.parse(localStorage.getItem("isRunning")) : false;
 runCount = localStorage.getItem("runCount") ? Number(localStorage.getItem("runCount")) : 0;
 runLimit = localStorage.getItem("runLimit") ? Number(localStorage.getItem("runLimit")) : 0;
@@ -24,8 +29,10 @@ delay = localStorage.getItem("delay") ? Number(localStorage.getItem("delay")) : 
 
     setTimeout(() => {
         neoIframe = document.querySelector("#neoIframe")
+        iframeDocument = neoIframe.contentDocument;
         if(neoIframe && isRunning){ 
             console.log("CURRENT RUN:" , runCount)
+            pushCurrentFrameToLog();
             iterateOnce();
         }
         else{
@@ -38,19 +45,47 @@ delay = localStorage.getItem("delay") ? Number(localStorage.getItem("delay")) : 
 })();
 }
 
-const iterateOnce = () => {
-    console.log("hello? anybody home", runCount, runLimit)
+const pushCurrentFrameToLog = () => {
+    table = iframeDocument.querySelector(".dc-two  table");
+    if(table !== null){
+        let tableText = table.innerText
+        const formattedText = tableText.replace(/\n/g, '__').replace(/\t/g, '_')
+        const endIndex = formattedText.indexOf("Edit Fields for Sync");
+        const trimmedText = formattedText.substring(0, endIndex).trim();
+    
+        logData = logData.concat(trimmedText + '|')
+        localStorage.setItem("logData",logData)
+        
+    }
+}
 
-    iframeDocument = neoIframe.contentDocument;
+function downloadLogAsFile() {
+    const data = JSON.stringify(logData.split("|"));
+    const blob = new Blob([data], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+  
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = "QBDErrorDestroyerLog";
+  
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  
+    URL.revokeObjectURL(url);
+  }
+  
+
+const iterateOnce = () => {
+
     allowExportButton = iframeDocument.querySelector("#allowExport") //unsused.
     objectIntegratedButton = iframeDocument.querySelector("#objIntegrated")
     nextButton = iframeDocument.querySelector(`button[name="next"]`)
 
 
     if(runCount < runLimit){
-        console.log("So I tried to run here.")
         if(nextButton && objectIntegratedButton){
-            objectIntegratedButton.checked == false ? objectIntegratedButton.click() : console.log("already unchecked")
+            objectIntegratedButton.checked == false ? objectIntegratedButton.click() :
             setTimeout(() => {
                 updateRuncountText(runCount+1, runLimit);
                 localStorage.setItem("runCount",runCount+1)
@@ -90,10 +125,13 @@ const evaluatePopupMessage = (type, value) => {
             console.log("reset from popup")
             localStorage.setItem("runCount", 0)
             runCount = 0;
+            localStorage.setItem("logData", "")
+            logData = ""
             localStorage.setItem("isRunning", false)
             break;
         case "log":
             console.log("log opened from popup (not working yet)")
+            downloadLogAsFile();
             break;
         default:
             console.log("invalid command")
@@ -112,12 +150,11 @@ chrome.runtime.onMessage.addListener(
 );
 
 const updateRuncountText = (newValue, limit) => {
-    console.log("just tried to send a message lol")
     chrome.runtime.sendMessage({
         popupRequestType: "updateRuncount",
         data: {newValue,limit}
     }, function(response) {
-        console.log(response.farewell);
+        // console.log(response.farewell);
       });
 }
 
@@ -129,7 +166,7 @@ let config = { attributes: false, childList: true, subtree: true };
 let observerCallback = function(mutationsList, observer) {
     for(let mutation of mutationsList) {
         if (mutation.type === 'childList') {
-            console.log('A child node has been added or removed.');
+            // console.log('A child node has been added or removed.');
             destroyer();
         }
     }
